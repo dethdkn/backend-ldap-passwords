5d33009b45fef2083c4760d48477428ac3c68f6d
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-11 01:08:43 -0300

    🐛 added cors

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,3 +1,10 @@
 export default defineEventHandler((event) => {
-	setHeaders(event, { 'Access-Control-Allow-Origin': 'https://backend.ldap-passwords.com', 'Access-Control-Allow-Methods': 'GET, POST', 'Access-Control-Allow-Headers': 'Content-Type' })
+	setHeaders(event, {
+		'Access-Control-Allow-Origin': 'Same-Origin',
+		'crossOriginResourcePolicy': 'same-origin',
+		'crossOriginOpenerPolicy': 'same-origin',
+		'crossOriginEmbedderPolicy': 'require-corp',
+		'contentSecurityPolicy': 'default-src \'self\';base-uri \'self\';font-src \'self\' https: data:;form-action \'self\';frame-ancestors \'self\';img-src \'self\' data:;object-src \'none\';script-src \'self\';script-src-attr \'none\';style-src \'self\' https: \'unsafe-inline\';upgrade-insecure-requests',
+		'X-XSS-Protection': 1,
+	})
 })
