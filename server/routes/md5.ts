4b01860e6ac8ba939c20b1be1a2938a27b26af52
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-11 01:40:33 -0300

    🐛 added cors

--- server/routes/md5.ts
+++ server/routes/md5.ts
@@ -1,6 +1,7 @@
 import { md5 } from 'ldap-md5'
 
 export default defineEventHandler(async (event) => {
+	setResponseHeaders(event, { 'Access-Control-Allow-Origin': '*' })
 	const { password } = getQuery(event) as { password: string }
 	return md5(password)
 })
--- server/routes/sha512crypt.ts
+++ server/routes/sha512crypt.ts
@@ -1,6 +1,7 @@
 import { sha512Crypt } from 'ldap-sha512'
 
 export default defineEventHandler(async (event) => {
+	setResponseHeaders(event, { 'Access-Control-Allow-Origin': '*' })
 	const { password, salt } = getQuery(event) as { password: string, salt: string }
 	return sha512Crypt(password, salt)
 })
