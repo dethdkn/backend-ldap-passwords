0d06604692bb798ab002c357c247b9d5f615d0a9
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-11 01:16:18 -0300

    🐛 added cors

--- server/routes/verifymd5.ts
+++ server/routes/verifymd5.ts
@@ -1,6 +1,7 @@
 import { verifyMD5 } from 'ldap-md5'
 
 export default defineEventHandler(async (event) => {
+	setResponseHeaders(event, { 'Access-Control-Allow-Origin': '*' })
 	const { cryptPassword, password } = (await readBody(event)) as { cryptPassword: string, password: string }
 	return verifyMD5(password, cryptPassword)
 })
--- server/routes/verifysha512.ts
+++ server/routes/verifysha512.ts
@@ -1,6 +1,7 @@
 import { verifySha512 } from 'ldap-sha512'
 
 export default defineEventHandler(async (event) => {
+	setResponseHeaders(event, { 'Access-Control-Allow-Origin': '*' })
 	const { cryptPassword, password } = (await readBody(event)) as { cryptPassword: string, password: string }
 	return verifySha512(password, cryptPassword)
 })
