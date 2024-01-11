4604cc0966bb40cfbafb5364a08e2ee833e2687c
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-11 01:37:44 -0300

    🐛 Switched body to query

--- server/routes/verifymd5.ts
+++ server/routes/verifymd5.ts
@@ -1,7 +1,6 @@
 import { verifyMD5 } from 'ldap-md5'
 
 export default defineEventHandler(async (event) => {
-	setResponseHeaders(event, { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST', 'Access-Control-Allow-Headers': 'Content-Type', 'Access-Control-Allow-Credentials': 'true' })
-	const { cryptPassword, password } = (await readBody(event)) as { cryptPassword: string, password: string }
+	const { cryptPassword, password } = getQuery(event) as { cryptPassword: string, password: string }
 	return verifyMD5(password, cryptPassword)
 })
--- server/routes/verifysha512.ts
+++ server/routes/verifysha512.ts
@@ -1,7 +1,6 @@
 import { verifySha512 } from 'ldap-sha512'
 
 export default defineEventHandler(async (event) => {
-	setResponseHeaders(event, { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST', 'Access-Control-Allow-Headers': 'Content-Type', 'Access-Control-Allow-Credentials': 'true' })
-	const { cryptPassword, password } = (await readBody(event)) as { cryptPassword: string, password: string }
+	const { cryptPassword, password } = getQuery(event) as { cryptPassword: string, password: string }
 	return verifySha512(password, cryptPassword)
 })
